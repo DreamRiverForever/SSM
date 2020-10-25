17254895c454191e68f41f0200c6463ace29bc2f
DreamRiverForever <1873348247@qq.com>
2020-10-25 16:16:34 +0800

    增加了产品添加功能，修复了日期转换错误的bug

--- heima_ssm_dao/src/main/java/com/itheima/dao/IProductDao.java
+++ heima_ssm_dao/src/main/java/com/itheima/dao/IProductDao.java
@@ -1,11 +1,17 @@
 package com.itheima.dao;
 
 import com.itheima.domain.Product;
+import org.apache.ibatis.annotations.Insert;
 import org.apache.ibatis.annotations.Select;
 
 import java.util.List;
 
 public interface IProductDao {
+    //查询所有产品
     @Select("select * from product")
     List<Product> findAll();
+
+    //保存产品信息
+    @Insert("insert into product(productNum,productName,cityName,departureTime,productPrice,productDesc,productStatus) values(#{productNum},#{productName},#{cityName},#{departureTime},#{productPrice},#{productDesc},#{productStatus})")
+    void save(Product product);
 }
--- heima_ssm_web/src/main/java/com/itheima/controller/ProductController.java
+++ heima_ssm_web/src/main/java/com/itheima/controller/ProductController.java
@@ -5,7 +5,9 @@ import com.itheima.domain.Product;
 import com.itheima.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -16,15 +18,23 @@ public class ProductController {
 
     @Autowired
     private IProductService iProductService;
-
+    //查询所有产品
     @RequestMapping("/findAll.do")
     public ModelAndView finaAll() throws Exception{
         ModelAndView modelAndView = new ModelAndView();
         List<Product> products = iProductService.findAll();
         modelAndView.addObject("productList",products);
         //System.out.println(products);
-        modelAndView.setViewName("product-list");
+        modelAndView.setViewName("product-list1");
         return modelAndView;
+    }
+
+    //产品添加
+    @RequestMapping("/save.do")
+    public String save(Product product) throws Exception{
+        System.out.println(product);
+        iProductService.save(product);
+        return "redirect:findAll.do";
 
 
     }
