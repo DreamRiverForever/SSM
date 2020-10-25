920dbeea426f7833352489373df9aaf7a881b2a2
DreamRiverForever <1873348247@qq.com>
2020-10-25 19:47:15 +0800

    查询订单并且配置pageHelper分页查询

---
+++ heima_ssm_service/src/main/java/com/itheima/service/Impl/OrdersServiceImpl.java
@@ -0,0 +1,26 @@
+package com.itheima.service.Impl;
+
+import com.github.pagehelper.PageHelper;
+import com.itheima.dao.IOrdersDao;
+import com.itheima.domain.Orders;
+import com.itheima.service.IOrdersService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
+public class OrdersServiceImpl implements IOrdersService {
+
+    @Autowired
+    private IOrdersDao ordersDao;
+
+    @Override
+    public List<Orders> findAll(int page, int size) {
+        //pageNum页码，pageSize每页显示的条数
+        PageHelper.startPage(page,size);
+        return ordersDao.findAll();
+    }
+}
